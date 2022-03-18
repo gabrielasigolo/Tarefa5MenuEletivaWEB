2c5091828b0dbedc7f5c1a628eab67d85f0f9251
gabrielasigolo <gabrielasigolo12@gmail.com>
2022-03-18 11:09:35 -0300

    inicio do back-end

--- scripts.js
+++ scripts.js
@@ -4,22 +4,16 @@ var quantity = document.getElementById('counter');
 var currentNumber = 0;
 
 function increment(){
-    if (currentNumber == 10) {
-        btnPlus.disabled = true;
-        btnMinus.disabled = false;
-    } else {
         currentNumber++;
         quantity.innerHTML = currentNumber;
-    }
-
-    
 }
 
 function decrement(){
-    if (currentNumber == -3){
+    if (currentNumber == 0){
         btnMinus.disabled = true;
         btnPlus.disabled = false;
     } else {
+        btnMinus.disabled = false;
         currentNumber--;
         quantity.innerHTML = currentNumber;
     }
