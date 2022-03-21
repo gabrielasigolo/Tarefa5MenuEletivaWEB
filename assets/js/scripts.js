e6f4f2beb43a10f4c1e659e4057945ebaa6e3707
gabrielasigolo <gabrielasigolo12@gmail.com>
2022-03-21 20:05:56 -0300

    botão de incrementar e decrementar

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,28 @@
-var btnPlus = document.getElementById('plus');
-var btnMinus = document.getElementById('minus');
-var quantity = document.getElementById('counter');
+var btnPlus = document.getElementsByClassName('plus');
+var btnMinus = document.getElementsByClassName('minus');
+var quantity = document.getElementsByClassName('quantity');
+var plusbuttonsCount = btnPlus.length;
+var minusbuttonsCount = btnMinus.length;
+var quantityCount = quantity.length;
 var currentNumber = 0;
+
+for(var i = 0; i < plusbuttonsCount; i+=1){
+    btnPlus[i].onclick = function increment(){
+             currentNumber++;
+             for(var j = 0; j < quantityCount; j+=1){
+                quantity[j].innerHTML = currentNumber;
+             }
+    };
+}
+
+for(var i = 0; i < minusbuttonsCount; i+=1){
+    btnMinus[i].onclick = function decrement(){
+            currentNumber--;
+            for(var j = 0; j < quantityCount; j+=1){
+                quantity[j].innerHTML = currentNumber;
+            }
+    }
+}
 
 function increment(){
     currentNumber++;
